fb9a88a73f0a3e3d2f1505c77f200905bad2cd32
Seiji Matsuoka <mojaie@aol.com>
2019-03-05 12:07:59 +0900

    wip

--- src/geometry/force.jl
+++ src/geometry/force.jl
@@ -7,15 +7,25 @@ export
     forcedirected
 
 
+mutable struct ForceConstraints
+    mincycle::Dict{Int,Int} # node, minimum cycle size
+
+    function ForceConstraints(cyclesize)
+        mincyc = Dict(i => maximum(c) for (i, c) in enumerate(cyclesize))
+        new(mincyc)
+    end
+end
+
+
 mutable struct ForceDirectedState{G<:UDGraph}
     graph::G
+    constraints::ForceConstraints
 
     velo::Matrix{Float64}
     force::Matrix{Float64}
     maxiter::Int
     tick::Float64
     stdlength::Float64
-    stdangle::Float64
     repulsion::Float64
     spring::Float64
     hinge::Float64
@@ -23,16 +33,16 @@ mutable struct ForceDirectedState{G<:UDGraph}
 
     coords::Matrix{Float64}
 
-    function ForceDirectedState{G}(graph, init) where {G<:UDGraph}
+    function ForceDirectedState{G}(graph, init, constraints) where {G<:UDGraph}
         state = new()
         initmat = rawdata(init)
         state.coords = deepcopy(initmat)
+        state.constraints = constraints
         state.velo = zeros(size(initmat))
         state.force = zeros(size(initmat))
         state.maxiter = 50
         state.tick = 0.01
         state.stdlength = 1.0
-        state.stdangle = -2/3
         state.repulsion = 0.01
         state.spring = 2.0
         state.hinge = 0.002
@@ -42,63 +52,59 @@ mutable struct ForceDirectedState{G<:UDGraph}
 end
 
 
-mutable struct ForceConstraints
-    angle::Vector{Tuple{Int,Int,Int,Float64}} # center, end1, end2, cos(angle)
-
-    function Constraints(graph, cycles, cycleedgesets)
-        angles = Tuple{Int,Int,Int,Float64}[]
-        for n in nodekeys(graph)
-            for ((a, ea), (b, eb)) in combinations(neighbors(graph, n))
-                c = intersect(cycleedgesets[ea], cycleedgesets[eb])
-                size = length(cycles[pop!(c)])
-                push!(consts, (n, a, b, cos(pi - 2pi / size))
-            end
-        end
-        new(angles)
-    end
-end
-
-
 """
     forcedirected(graph::UDGraph) -> Cartesian2D
 
 Compute 2D embedding based on the graph distance.
 """
-function Geometry.forcedirected(
+function forcedirected(
         graph::G, init::Cartesian2D, constraints::ForceConstraints
         ) where {G<:UDGraph}
-    state = ForceDirectedState{G}(graph, init)
+    state = ForceDirectedState{G}(graph, init, constraints)
     t = state.tick
     for i in 1:state.maxiter
-        energy = 0.0
+        # Calculate forces
+        f = zeros(size(state.coords))
         for n1 in nodekeys(graph)
-            p1v = state.velo[n1, :] * t
-            p1a = state.force[n1, :] * (t ^ 2) * 0.5
-            state.coords[n1, :] +=  p1v + p1a
             p1 = state.coords[n1, :]
-            force = [0.0, 0.0]
             # Repulsion constraints
             for n2 in nodekeys(graph)
                 n2 == n1 && continue
                 p2 = state.coords[n2, :]
                 factor = min(state.repulsion / norm(p2 - p1) ^ 2, state.repulsion * 100)
-                force -= factor * normalize(p2 - p1)
+                f[n1, :] -= factor * normalize(p2 - p1)
             end
             # Spring constraint
             for nbr in neighborkeys(graph, n1)
                 pnbr = state.coords[nbr, :]
                 factor = (norm(pnbr - p1) - state.stdlength) * state.spring
-                force += factor * normalize(pnbr - p1)
+                if degree(graph, n1) > 2
+                    factor *= 2 # TODO This can work?
+                end
+                f[n1, :] += factor * normalize(pnbr - p1)
             end
             # Hinge constraint
-            for (a, b) in combinations(neighbors(graph, n1))
-                pa = state.coords[a, :]
-                pb = state.coords[b, :]
-                factor = (interiorangle(pa, pb) - state.stdangle) * state.hinge
-                force += factor * normalize(p2 - p1)
+            if degree(graph, n1) == 2
+                (a, b) = neighborkeys(graph, n1)
+                p1a = state.coords[a, :] - p1
+                p1b = state.coords[b, :] - p1
+                minc = state.constraints.mincycle[n]
+                ang = cos(pi - 2pi / minc)
+                cross = p1a[1] * p1b[2] - p1b[1] * p1a[2]
+                direction = cross >= 0 ? -1 : 1
+                factor = (dot(p1a, p1b) / (norm(p1a) * norm(p1b)) - ang) * state.hinge
+                normveca = rotate(normalize(p1a - p1), pi / 2 * direction)
+                f[a, :] += factor * normvec
+                normvecb = rotate(normalize(p1b - p1), pi / 2 * direction)
+                f[b, :] += factor * normvec
             end
-            state.velo[n1, :] += (state.force[n1, :] + force) / 2 * t
-            state.force[n1, :] = force
+        end
+        # Apply forces
+        energy = 0.0
+        for n1 in nodekeys(graph)
+            state.velo[n1, :] += (state.force[n1, :] + f[n1, :]) / 2 * t
+            state.force[n1, :] = f[n1, :]
+            state.coords[n1, :] += state.velo[n1, :] * t + state.force[n1, :] * (t ^ 2) * 0.5
             energy += norm(state.force[n1, :])
         end
         println(energy)
